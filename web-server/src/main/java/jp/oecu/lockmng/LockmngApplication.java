8aeeeb26a6a578884c2b29074b1c376b228003dc
Naga0915 <gp23a098@oecu.jp>
2025-05-05 13:11:38 +0900

    追加：Spring Bootアプリケーション 追加：Docker, Docker Composeサポート

---
+++ web-server/src/main/java/jp/oecu/lockmng/LockmngApplication.java
@@ -0,0 +1,13 @@
+package jp.oecu.lockmng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LockmngApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LockmngApplication.class, args);
+	}
+
+}
---
+++ web-server/src/test/java/jp/oecu/lockmng/LockmngApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.oecu.lockmng;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LockmngApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
