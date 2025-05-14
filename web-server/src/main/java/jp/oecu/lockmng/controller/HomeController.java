e02d8228a1e6fdf19d81a4513ce5170acabd284e
Naga0915 <gp23a098@oecu.jp>
2025-05-14 17:00:24 +0900

    追加：postgresqlの追加

--- web-server/src/main/java/jp/oecu/lockmng/controller/HomeController.java
+++ web-server/src/main/java/jp/oecu/lockmng/controller/HomeController.java
@@ -3,13 +3,18 @@ package jp.oecu.lockmng.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+
+import jakarta.servlet.http.HttpSession;
 
 
 @Controller
 public class HomeController {
     @GetMapping("/")
-    public String getMethodName(@RequestParam(defaultValue = "guest") String name, Model model) {
+    public String getMethodName(HttpSession session, Model model) {
+        String name = "guest";
+        if(session.getAttribute("session_id") != null){
+            name = String.format("%d", (int)session.getAttribute("session_id"));
+        }
         model.addAttribute("name", "["+name+"]");
         return "index.html";
     }
--- web-server/src/main/java/jp/oecu/lockmng/controller/UserAccessController.java
+++ web-server/src/main/java/jp/oecu/lockmng/controller/UserAccessController.java
@@ -3,16 +3,20 @@ package jp.oecu.lockmng.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import jakarta.servlet.http.HttpSession;
+
 
 @Controller
 public class UserAccessController {
     @GetMapping("/login")
-    public String login() {
+    public String login(HttpSession session) {
+        session.setAttribute("session_id", 123);
         return "login.html";
     }
     
     @GetMapping("/logout")
-    public String logout(){
+    public String logout(HttpSession session){
+        session.removeAttribute("session_id");
         return "logout.html";
     }
 }
