56f52eec5a4869972dcb2c8eaa36ea174faceaed
Naga0915 <gp23a098@oecu.jp>
2025-06-06 16:58:00 +0900

    追加：予約リポジトリ

--- web-server/src/main/java/jp/oecu/lockmng/controller/ReserveController.java
+++ web-server/src/main/java/jp/oecu/lockmng/controller/ReserveController.java
@@ -1,11 +1,18 @@
 package jp.oecu.lockmng.controller;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import jp.oecu.lockmng.model.NewReserveModel;
+import jp.oecu.lockmng.util.CustomUserDetails;
+
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class ReserveController {
@@ -15,8 +22,16 @@ public class ReserveController {
     }
 
     @PostMapping("/resv")
-    public String reservePost(@RequestBody String entity) {
-        
-        return entity;
+    public String reservePost(@AuthenticationPrincipal CustomUserDetails userDetails, @ModelAttribute NewReserveModel newReserveModel, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
+        if(bindingResult.hasErrors()){
+            StringBuilder sb = new StringBuilder();
+            for (FieldError e : bindingResult.getFieldErrors()) {
+                sb.append(e.getDefaultMessage() + "\n");
+            }
+            redirectAttributes.addFlashAttribute("msg", bindingResult);
+            return "redirect:/resv";
+        }
+        //サービスの処理
+        return "redirect:/resv";
     }
 }
